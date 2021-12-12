1b3f364d622771d907c262ac310f02e69184cb41
Quentin KREMPP <quentin.krempp@ensta-paris.fr>
2021-12-12 19:34:33 +0100

    Remove temporary files

--- .Corrected-Chess/Chess_Interface.py
+++
@@ -1,9 +0,0 @@
-from subprocess import Popen, PIPE
-#Or just:
-coups = open("Game.txt", 'r').read().split(sep='\n')[:-1]
-args = ["./Chess.xpp"] + coups
-process = Popen(args, stdout=PIPE)
-process.wait()
-for output in process.stdout.read().strip().decode().splitlines():
-    print(output)
-#(stdout, stderr) = process.communicate()
